b5eea5bb29b82e61fd827a30c61cd1f815d69bc7
Ivanov Serhii <ivanovsergnovbug@gmail.com>
2025-08-07 16:53:19 +0300

    Configured employees Model

--- employees/models.py
+++ employees/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from restaurant.models import Restaurant
+from django.contrib.auth.models import User
+
 
 class Employee(models.Model):
-    pass
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE,
+        verbose_name='Restaurant', null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, 
+        verbose_name='User', null=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name} employee'
