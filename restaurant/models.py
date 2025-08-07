567d5553607807fd63fd6acd9e7b803fbe360b0f
Ivanov Serhii <ivanovsergnovbug@gmail.com>
2025-08-07 17:06:25 +0300

    Modified models 1

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+
 class Restaurant(models.Model):
     name = models.CharField(verbose_name='Restaurant name', max_length=30, blank=False)
     menu_of_the_day = models.OneToOneField('Menu', related_name='featured_in_restaurant', 
-        on_delete=models.DO_NOTHING, verbose_name='Menu of the day')
+        on_delete=models.DO_NOTHING, verbose_name='Menu of the day', blank=True)
     user = models.OneToOneField(User, related_name='restaurant_model',
-        on_delete=models.PROTECT, verbose_name='Representing user')
+        on_delete=models.PROTECT, verbose_name='Representing user', blank=True)
+
+    def __str__(self):
+        return f'{self.name} restaurant'
 
 
 class Menu(models.Model):
@@ -15,6 +19,9 @@ class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, related_name='menus', on_delete=models.CASCADE,
         verbose_name='Restaurant', blank=False)
     day = models.DateField(verbose_name='Day', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f'Menu {self.name} of {self.restaurant.name} restaurant'
 
 # Non-unique dishes for testing purposes
 class Dish(models.Model):
@@ -24,10 +31,13 @@ class Dish(models.Model):
     # blank=True set for image for testing purposes
     image = models.ImageField(verbose_name='Dish image', blank=True)
 
+    def __str__(self):
+        return f'Dish {self.name}'
+
 
 class Vote(models.Model):
     employee = models.ForeignKey('employees.Employee', related_name='votes',
-        blank=True, verbose_name='Employee', on_delete=models.CASCADE)
+        blank=False, verbose_name='Employee', on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, related_name='votes',
         blank=False, verbose_name='Menu', on_delete=models.CASCADE)
     
@@ -35,3 +45,6 @@ class Vote(models.Model):
     def clean(self):
         if Vote.objects.filter(employee=self.employee, menu__day=self.menu.day).exclude(pk=self.pk).exists():
             raise ValidationError('You have already voted for this day.')
+        
+    def __str__(self):
+        return f'{self.employee} voted for {self.menu.name} menu'
